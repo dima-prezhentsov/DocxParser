909dbd4bc0fe1e341be39700e9ea699ec2b9a340
dima-prezhentsov <dimonpre@gmail.com>
2022-11-04 14:43:34 +0300

    first commit

--- main.py
+++ main.py
@@ -3,19 +3,6 @@ import codeParser
 
 
 def main():
-
-    # path = "C:\\Users\\dimon\\Desktop\\labBuffer\\src\\main\\java\\banks\\account\\Account.java".split('\\')[:-1]
-    # fileName = "C:\\Users\\dimon\\Desktop\\labBuffer\\src\\main\\java\\banks\\account\\Account.java".split('\\')[-1]
-    # print("\\".join(path))
-    # print(fileName)
-    # try:
-    #     os.makedirs("C:\\Users\\dimon\\Desktop\\labBuffer\\src\\main\\java\\banks\\entity")
-    # except:
-    #     file = open("C:\\Users\\dimon\\Desktop\\labBuffer\\src\\main\\java\\banks\\account\\Account.java", 'w+')
-    #     file.write("123")
-    # os.makedirs("C:\\Users\\dimon\\Desktop\\labBuffer\\src\\main\\java\\banks\\entity")
-    # testFile = open("C:\\Users\\dimon\\Desktop\\labBuffer\\src\\main\\java\\banks\\account\\Account.java", "w+")
-    # testFile.write("123")
     docPath = "C:\\Users\\dimon\\Desktop\\techReports\\Отчет1ЛабораторнаяПреженцовДмитрийИгоревичМ32051.docx"
     codeFiles = codeParser.parse(docPath)
 
@@ -30,9 +17,5 @@ def main():
                     file.write(code)
 
 
-
-
 if __name__ == '__main__':
-    # print(os.path.join("C:/Users/dimon/Desktop/labBuffer", "labBuffer/src/main/java/banks/account"))
-    # os.makedirs(os.path.join("C:/Users/dimon/Desktop/labBuffer", "src/main/java/banks/account"))
     main()
